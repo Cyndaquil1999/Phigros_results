31a9648f63533a617a0a1bba080dc41a5866ee81
cyndaquil1999 <mcah8278@yahoo.co.jp>
2024-06-01 10:24:08 +0900

    トップページの作成

--- phigrosTools/frontend/src/app/_component/header.tsx
+++ phigrosTools/frontend/src/app/_component/header.tsx
@@ -3,7 +3,7 @@ import { Button } from "../../components/ui/button";
 
 export function Header() {
   return (
-    <div className="flex justify-between items-center">
+    <div className="flex justify-between items-center ">
       <div className="text-xl">Phigrosレート計算ツール</div>
       <div className="flex space-x-2 sm:space-x-4 md:space-x-6 lg:space-x-8">
         <Button>
--- phigrosTools/frontend/src/app/page.tsx
+++ phigrosTools/frontend/src/app/page.tsx
@@ -1,9 +1,37 @@
 import { Header } from "./_component/header";
+import { Button } from "../components/ui/button";
+import Link from "next/link";
 
 export default function Page() {
   return (
     <div>
       <Header />
+      <div className="flex flex-col justify-center items-center mb-10">
+        <div className="text-xl mb-4">このツールでは以下の機能があります。</div>
+        <div className="flex flex-col space-y-2 text-lg">
+          <div className="flex">
+            <span>1.</span>
+            <span> Phigrosのレート計算</span>
+          </div>
+          <div className="flex">
+            <span>2.</span>
+            <span> ハイスコアを登録・保存</span>
+          </div>
+          <div className="flex">
+            <span>3.</span>
+            <span> レート上げにおすすめの譜面推薦</span>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col justify-center items-center space-y-4">
+        <Button className="w-32 sm:w-40 md:w-48 lg:w-56">
+          <Link href="/signin">ログイン</Link>
+        </Button>
+        <Button className="w-32 sm:w-40 md:w-48 lg:w-56">
+          <Link href="/signup">会員登録</Link>
+        </Button>
+      </div>
     </div>
   );
 }
