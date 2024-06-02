5d26a38fdb394c00ec4f04c5aa4f57b1d9d06919
cyndaquil1999 <mcah8278@yahoo.co.jp>
2024-06-02 15:42:52 +0900

    shadcnのテーブルを追加

--- phigrosTools/frontend/src/app/rating/page.tsx
+++ phigrosTools/frontend/src/app/rating/page.tsx
@@ -1,21 +1,24 @@
 import { Header } from "../_component/header";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/src/components/ui/table";
 
 export default function Rating() {
   return (
     <div>
       <Header />
-      <div className="flex-col justify-center items-center my-10">
+      <div className="flex flex-col justify-center items-center my-10">
         <div className="text-5xl">レート計算</div>
-        <div className="flex-col">
-          <div>レートの計算方法は</div>
-        </div>
       </div>
 
       {/* TODO: shadcnのテーブルを使ってここにべ枠と計算値を表示させる */}
       <div>
-        <div className="flex justify-center items-center space-y-4">
-          <div></div>
-        </div>
       </div>
     </div>
   );
