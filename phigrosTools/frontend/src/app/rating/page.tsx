7b0a03bb67a4f5959fa323275d5263dddc20c159
cyndaquil1999 <mcah8278@yahoo.co.jp>
2024-06-13 13:10:23 +0900

    rating計算ページの作成

--- phigrosTools/frontend/src/app/rating/page.tsx
+++ phigrosTools/frontend/src/app/rating/page.tsx
@@ -1,24 +1,51 @@
-import { Header } from "../_component/header";
+"use client";
+
+import { Header } from "app/_component/header";
 import {
   Table,
   TableBody,
-  TableCaption,
   TableCell,
   TableHead,
   TableHeader,
   TableRow,
-} from "@/src/components/ui/table";
+} from "../../components/ui/table";
+import { useEffect } from "react";
 
-export default function Rating() {
+export default function ScorePage() {
+  useEffect(() => {
+    fetch("api/test")
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
+
   return (
     <div>
       <Header />
-      <div className="flex flex-col justify-center items-center my-10">
-        <div className="text-5xl">レート計算</div>
+      <div className="flex justify-center text-5xl">Score</div>
+      <div className="mt-12 flex justify-center">
+        <p className="text-3xl">Your Ranking Score:　15.52</p>
       </div>
-
-      {/* TODO: shadcnのテーブルを使ってここにべ枠と計算値を表示させる */}
-      <div>
+      <div className="mt-12 mx-12">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>MusicTitle</TableHead>
+              <TableHead>Composer</TableHead>
+              <TableHead>Difficulty</TableHead>
+              <TableHead>Accuracy(%)</TableHead>
+              <TableHead>RS Each Music</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell>DESTRUCTION 3,2,1</TableCell>
+              <TableCell>aaa</TableCell>
+              <TableCell>IN</TableCell>
+              <TableCell>99.86</TableCell>
+              <TableCell>15.77</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
       </div>
     </div>
   );
